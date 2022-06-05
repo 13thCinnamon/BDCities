008d6ec2a7657a2f3911ffbe5c0674f68652509d
Куц <kutzhenia03@gmail.com>
2022-06-05 11:52:00 +0600

    Initial commit

---
+++ Coords.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CitiesDataBase
+{
+    class Coords
+    {
+        public int Id { get; set; }
+        public double lat { get; set; }
+        public double lon { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace CitiesDataBase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = Path.Combine(Environment.CurrentDirectory, "json1.json");
+            var json = File.ReadAllText(path);
+            var jsonObjs = Newtonsoft.Json.JsonConvert.DeserializeObject<IEnumerable<City>>(json);
+
+            using (CitiesContext db = new CitiesContext())
+            {
+                db.Cities.AddRange(jsonObjs);
+                db.SaveChanges();
+                Console.WriteLine("Объекты успешно сохранены");
+            }
+        }
+    }
+}
